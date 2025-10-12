ed00e978ccf232ef16f02c4a54cf67831ae992ad
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2025-10-12 23:09:09 +0200

    initial

---
+++ ex11_fibonacci/src/test/java/eu/binarystars/tdd/fibonacci/FirstTest.java
@@ -0,0 +1,24 @@
+package eu.binarystars.tdd.fibonacci;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.*;
+
+class FirstTest {
+
+    @Test
+    void init() throws Exception{
+        // given
+        First first = new First();
+
+        // when
+        String actual = first.getEmail();
+
+        // then
+        Person expected = new Person();
+        assertThat(actual).isNull();
+    }
+}
