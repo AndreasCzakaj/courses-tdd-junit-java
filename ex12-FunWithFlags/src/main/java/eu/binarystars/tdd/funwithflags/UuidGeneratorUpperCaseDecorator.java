6128cdc71d8defbae93b17ce3eab656ce1c77aba
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2025-10-12 21:43:08 +0200

    ex21-uss/src/

---
+++ ex12-FunWithFlags/src/main/java/eu/binarystars/tdd/funwithflags/UuidGeneratorUpperCaseDecorator.java
@@ -0,0 +1,8 @@
+package eu.binarystars.tdd.funwithflags;
+
+public class UuidGeneratorUpperCaseImpl implements UuidGenerator {
+    @Override
+    public String create() {
+        return "";
+    }
+}
