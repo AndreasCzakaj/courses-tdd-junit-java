4956c75ced83a6aca359542c249e8175de412553
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2025-10-12 23:10:17 +0200

    initial

---
+++ ex11_fibonacci/src/main/java/eu/binarystars/tdd/fibonacci/Person.java
@@ -0,0 +1,12 @@
+package eu.binarystars.tdd.fibonacci;
+
+import java.util.Date;
+
+public class Person {
+    public int id;
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String ipAddress;
+    public Date birthday;
+}
