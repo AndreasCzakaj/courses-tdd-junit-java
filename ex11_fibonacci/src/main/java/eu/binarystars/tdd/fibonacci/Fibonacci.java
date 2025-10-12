f28abebd818e792f9954e7bc0264f15940782a84
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2025-10-12 21:39:56 +0200

    reset to initial state

--- ex11_fibonacci/src/main/java/eu/binarystars/tdd/fibonacci/Fibonacci.java
+++
@@ -1,7 +0,0 @@
-package eu.binarystars.tdd.fibonacci;
-
-public class Fibonacci {
-    public Integer calc(Integer index) {
-        return null;
-    }
-}
--- ex11_fibonacci/src/test/java/eu/binarystars/tdd/fibonacci/FibonacciTest.java
+++ ex11_fibonacci/src/test/java/eu/binarystars/tdd/fibonacci/FibonacciTest.java
@@ -1,6 +1,7 @@
 package eu.binarystars.tdd.fibonacci;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -8,90 +9,104 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.*;
+import static java.util.stream.Stream.of;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.*;
 
-class FibonacciTest {
-    Fibonacci fibonacci;
+public class FibonacciTest {
 
-    Fibonacci factory() {
-        return new Fibonacci();
-    }
-
-    @BeforeEach
-    void beforeEach() {
-        fibonacci = factory();
+    @Test
+    @Disabled
+    void itShouldThrowForNullIndex() {
+        fail("impl me");
     }
 
     @Test
-    @org.junit.jupiter.api.Disabled
-    void itShouldYield0For0() {
-        // given
-        Integer index = 0;
-
-        // when
-        Integer actual = fibonacci.calc(index);
-
-        // then
-        Integer expected = 0;
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    @org.junit.jupiter.api.Disabled
+    @Disabled
     void itShouldThrowForNegativeIndex() {
-        // given
-        Integer index = -1;
-
-        // when
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> fibonacci.calc(index));
-
-        // then
-        assertThat(e.getMessage()).contains("index must be >= 0");
+        fail("impl me");
     }
 
-
-    @ParameterizedTest(name = "it should yield {1} for index #{0}")
-    @MethodSource("shouldPassParams")
-    @org.junit.jupiter.api.Disabled
-    void shouldPass(Integer index, Integer expected) {
-        Integer actual = fibonacci.calc(index);
-        assertThat(actual).isEqualTo(expected);
+    @Test
+    @Disabled
+    void itShouldThrowForIndexAbove46() {
+        fail("impl me");
     }
 
-    static Stream<Arguments> shouldPassParams() {
-        // https://www.wackerart.de/mathematik/big_numbers/fibonacci_numbers.html
-        return Stream.of(
-                arguments(0, 0)
-                //arguments(1, 1)
-                //arguments(2, 1)
-                //arguments(3, 2)
-                //arguments(4, 3)
-                //arguments(5, 5)
-                //arguments(6, 8)
-                //arguments(19, 4_181)
-                //arguments(40, 102_334_155)
-                //arguments(46, 1_836_311_903)
-        );
+    // see https://www.wackerart.de/mathematik/big_numbers/fibonacci_numbers.html
+
+    @Test
+    @Disabled
+    void itShouldYield0ForIndex0() {
+        fail("impl me");
     }
 
-
-    @ParameterizedTest(name = "it should throw IllegalArgumentException with message `{1}` for index #{0}")
-    @MethodSource("shouldFailParams")
-    @org.junit.jupiter.api.Disabled
-    void shouldFail(Integer index, String expected) {
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, () -> fibonacci.calc(index));
-        assertThat(e.getMessage()).contains(expected);
+    @Test
+    @Disabled
+    void itShouldYield1ForIndex1() {
+        fail("impl me");
     }
 
-    static Stream<Arguments> shouldFailParams() {
-        return Stream.of(
-                arguments(null, "index must not be null")
-                //arguments(-1, "index must be >= 0")
-                //arguments(47, "index must be <= 46")
-        );
+    @Test
+    @Disabled
+    void itShouldYield1ForIndex2() {
+        fail("impl me");
     }
+
+    @Test
+    @Disabled
+    void itShouldYield2ForIndex3() {
+        fail("impl me");
+    }
+
+    @Test
+    @Disabled
+    void itShouldYield3ForIndex4() {
+        fail("impl me");
+    }
+
+    @Test
+    @Disabled
+    void itShouldYield5ForIndex5() {
+        fail("impl me");
+    }
+
+    @Test
+    @Disabled
+    void itShouldYield8ForIndex6() {
+        fail("impl me");
+    }
+
+    @Test
+    @Disabled
+    void itShouldYield55ForIndex10() {
+        fail("impl me");
+    }
+
+    @Test
+    @Disabled
+    void itShouldYield6_765ForIndex20() {
+        fail("impl me");
+    }
+
+    @Test
+    @Disabled
+    void itShouldYield832_040ForIndex30() {
+        fail("impl me");
+    }
+
+    @Test
+    @Disabled
+    void itShouldYield102_334_155ForIndex40() {
+        fail("impl me");
+    }
+
+    @Test
+    @Disabled
+    void itShouldYield1_836_311_903For46() {
+        fail("impl me");
+    }
+
 }
