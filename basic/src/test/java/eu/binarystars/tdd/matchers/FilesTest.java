22b5d39affd50ac7452d029063e53fe180ea187a
Andreas Czakaj <andreas.czakaj@binary-stars.eu>
2025-11-29 00:21:57 +0100

    add FilesTest

---
+++ basic/src/test/java/eu/binarystars/tdd/matchers/FilesTest.java
@@ -0,0 +1,85 @@
+package eu.binarystars.tdd.matchers;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+
+import java.io.*;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+
+class FilesTest {
+    File newFile;
+
+    @BeforeEach
+    void init(@TempDir File tmpDir) throws Throwable {
+        assertThat(tmpDir).isDirectory();
+        assertThat(tmpDir).exists();
+        newFile = new File(tmpDir, "newFile.txt");
+        assertThat(newFile).doesNotExist();
+
+        List<String> lines = Arrays.asList("The first line", "The second line");
+        Files.write(newFile.toPath(), lines, StandardCharsets.UTF_8);
+    }
+
+    @Test
+    void txtFile() throws Throwable {
+        assertThat(newFile).exists();
+        assertThat(newFile).content(StandardCharsets.UTF_8).contains("The first line");
+        assertThat(newFile).content(StandardCharsets.UTF_8).contains("The second line");
+    }
+
+    @Test
+    void jsonFile() throws Throwable {
+        ObjectMapper objectMapper = new ObjectMapper();
+        URL url = IOUtils.resourceToURL("/ppl.json");
+        File file = new File(url.getFile());
+        assertThat(file.exists()).isTrue();
+        var parser = objectMapper.createParser(url);
+        Person[] people = parser.readValueAs(Person[].class);
+        assertThat(people.length).isEqualTo(1000);
+
+
+
+        Person expectedPerson = new Person();
+        expectedPerson.id = 18;
+        expectedPerson.firstName = "Crawford";
+        expectedPerson.lastName = "Roisen";
+        expectedPerson.email = "croisenh@independent.co.uk";
+        expectedPerson.ipAddress = "163.170.23.182";
+
+        List<Person> pplList = Arrays.asList(people);
+        //assertThat(pplList).hasSize(1000).contains(expectedPerson);
+
+        Map<String, Object> actual = objectMapper.convertValue(people[17], new TypeReference<Map<String, Object>>() {
+        });
+        Map<String, Object> expected = objectMapper.convertValue(expectedPerson, new TypeReference<Map<String, Object>>() {
+        });
+        assertThat(actual).isEqualTo(expected);
+
+        assertThat(people[17]).usingRecursiveComparison().isEqualTo(expectedPerson);
+
+    }
+
+    @Test
+    void jsonFile2() throws Throwable {
+        URL url = IOUtils.resourceToURL("/ppl.json");
+        File file = new File(url.getFile());
+        assertThat(file.exists()).isTrue();
+        String json = Files.readString(file.toPath(), StandardCharsets.UTF_8);
+
+        assertThatJson(json).isArray().hasSize(1000);
+    }
+
+}
